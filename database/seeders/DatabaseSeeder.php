d6335d99b4970b3d285f20c12c90199ad7842295
Daniel Aguilar <aguilardaniel631@gmail.com>
2024-07-03 20:46:27 -0600

    cambios en los seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,16 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('duenios')->insert([
-            ['nombre' => 'Juan', 'apellido' => 'Pérez', 'correo' => 'juan@example.com', 'telefono' => '123456789'],
-            ['nombre' => 'Ana', 'apellido' => 'García', 'correo' => 'ana@example.com', 'telefono' => '987654321'],
-            ['nombre' => 'Luis', 'apellido' => 'Martínez', 'correo' => 'luis@example.com', 'telefono' => '456789123'],
-        ]);
-    
-        DB::table('propiedades')->insert([
-            ['piso' => 1, 'area' => 100.0, 'color' => 'Rojo', 'idDuenio' => 1],
-            ['piso' => 2, 'area' => 120.0, 'color' => 'Azul', 'idDuenio' => 2],
-            ['piso' => 3, 'area' => 150.0, 'color' => 'Verde', 'idDuenio' => 3],
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
